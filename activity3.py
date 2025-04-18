54a7495e8bb5833a823d251818550a3ce1cac1b6
peterisc00l <augustambaekwe@gmail.com>
2025-04-18 18:20:05 +0100

    Add files via upload

---
+++ activity3.py
@@ -0,0 +1,4 @@
+# Python program to calculate the sum of the first ten natural numbers
+# Use built-in sum and range funtions to calculate the sum
+total_sum = sum(range(1,11))
+print(f"The sum of the first ten natural numbers is {total_sum}")
---
+++ activity4.py
@@ -0,0 +1,6 @@
+# Python program to check if a number is prime 
+# Take input from user
+num = int(input("Enter a number: "))
+# Check number is greater than 1 (since the primes are > 1)
+if num > 1 :
+    # Loop only up to square root of num for efficiecy
