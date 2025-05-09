76269b9eafff194c5f0812f0abea3287d7f0eee6
peterisc00l <augustambaekwe@gmail.com>
2025-05-09 18:46:31 +0100

    Add files via upload

--- activity1.py
+++ activity1.py
@@ -1,12 +1,24 @@
-class Employee:
+# parent class
+class Person( object ):
+    # __init__ is know as the consturctor
+    def __init__(self, name, idnumber):
+        self.name = name
+        self.idnumber = idnumber
+    def display(self):
+      print(self.name)
+      print(self.idnumber)
 
-        # Initializing (Constructor)
-        def __init__(self):
-                print('Employee created')
+# child class
+class Employee( Person ):
+    def __init__(self, name, idnumber, salary, post):
+        self.salary = salary
+        self.post = post
 
-        # Deleting (Destructor)
-        def __del__(self):
-                print('Destructor called, Employee deleted')
+        # invoking the __init__ of the parent class
+        Person.__init__(self, name, idnumber)
 
-obj = Employee()
-del obj     
+# creation of an object variable or an instance
+a = Employee('Peter', 20131001, 645792, "Intern")
+
+# calling a fuction of the class Person using its instance
+a.display()
