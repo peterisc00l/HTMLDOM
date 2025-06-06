5122bf72a604384ec310deed4a2e6a65f64bd1b4
peterisc00l <augustambaekwe@gmail.com>
2025-06-06 17:59:00 +0100

    Add files via upload

--- activity1.py
+++ activity1.py
@@ -1,20 +1,57 @@
 from tkinter import *
+from tkinter import messagebox
+from PIL import Image, ImageTk
 
-window = Tk()
-window.title('Tkinker Sample Window')
-window.geometry('300x300')
+root = Tk()
+root.title('Denomination Counter')
+root.configure(bg='light  blue')
+root.geometry('650x400')
 
-greeting = Label(text="Hello User", fg='aquamarine', bg='blue')
-button = Button(text="Click me", bg='black', width=50)
-entry = Entry(fg="cyan", bg="blue", width=50)
-greeting.pack()
-button.pack()
-entry.pack()
+upload = Image.open('game.png')
+upload = upload.resize(300, 300)
+label = Label(root, image=Image, bg='light blue')
+label.place(x=180, y=20)
 
-frame = Frame(master=window, relief=RAISED, borderwidth=5)
-frame.pack()
-lable = Label(master=frame, text='Sample Frame')
-lable.pack()
+label = Label(root,
+              text="Hey User! Welcome to Denomination Counter Application.",
+              bg='light blue')
+label1.place(relx=0.5, y=340, anchor=CENTER)
 
-textbox = Text(fg='green', bg='yellow')
-textbox.pack()
+def msg():
+    MsgBox = messagebox.Showinfo(
+        "Alert", "Do you want to calculate the denomination count?")
+    if MsgBox =='ok':
+        topwin()
+
+button1 =Button(root,
+                text="Let's get started:"
+                command=msg,
+                bg='brown'
+                fg='white')
+button1.place(x=260, y=360)
+
+def topwin():
+    top = Toplevel()
+    top.title("Denomination Calculator")
+    top.configure(bg='aquamarine')
+    top.geometry("600x350+58+58")
+
+    label = Label(top, text="Enter total amount", bg='cyan')
+    entry = Entry(top)
+    lbl = label(top, text="Here are the number of each denominations", bg='blue')
+    
+    l1 =Label(top, text="2003", bg='gold')
+    l2 =Label(top, text="586", bg='red')
+    l3 =Label(top, text="410", bg='light blue')
+
+    t1 = Entry(top)
+    t2 = Entry(top)
+    t3 = Entry(top)
+
+def calculator():
+    try:
+        global amount
+        amount = int(entry.get())
+        note2000 = amount // 2000
+        amount %= 2000
+        note 
