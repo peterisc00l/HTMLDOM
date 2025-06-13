5067c36bcdf786e6e550eec037c814979c98e3b9
peterisc00l <augustambaekwe@gmail.com>
2025-06-13 18:53:29 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
-var fs = require('fs');
+var mysql = require('mysql');
 
-fs.unlink('afile2.txt', function (err) {
-    if (err) throw err;
-    console.log('File deleted!')
+var con = mysql.createConnection({
+    host:"remotemysql.com",
+    user:"Rz8hqn1dk4",
+    password:"nd0WKO3xeO",
+});
+
+con.connect(function(err) {
+    if(err) throw err;
+    console.log("Connected!")
 })
