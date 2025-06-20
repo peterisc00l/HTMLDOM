ecc611b3099c5a0a7addf1d835c0e06477607263
peterisc00l <augustambaekwe@gmail.com>
2025-06-20 19:05:57 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 var mysql = require('mysql');
 
-var con = mysql.createConnection({
+var connection = mysql.createConnection({
     host:"remotemysql.com",
     user:"Rz8hqn1dk4",
     password:"nd0WKO3xeO",
+    database:"Rz8hqn1dk4"
 });
-
-con.connect(function(err) {
+connection.connect((err)=>{
+    if(err) throw err
+    console.log("connected!");
+    var sql = "INSERT INTO(Student_ID,Student_FirstName,Student_LastName,Student_City,Student_Grade) VALUES ?";
+    var values = [
+        [101,'Teenu','Parshant','Madurai','5'],
+        [101,'Chintu','Prakash','Chennai','6'],
+        [101,'caral','Smith','Texas','2'],
+        [101,'Riya','Gupta','Pune','9'],
+        [101,'Teka','Parshant','Banglore','8'],
+    ];
+connection.query(sql,[values],function(err,result){
     if(err) throw err;
-    console.log("Connected!")
-})
+    console.log("Multiple Data inserted in DB");
+});
+});
